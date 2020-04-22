c35db45a9db721f3bb24b81dbd83c6c0185db842
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2020-04-22 15:11:50 +0200

    first

---
+++ objs.js
@@ -0,0 +1,18 @@
+User = {
+  id: 123,
+  name: Per Per,
+  groups: [1,2],
+  bookings: [obj1, obj2, obj3]
+}
+
+Car = {
+  id: 456,
+  modell: Saab 93
+}
+
+Booking = {
+  bookingId: id789,
+  user: id123,
+  car: id456,
+  time: "date xxx to date xxx"
+}
